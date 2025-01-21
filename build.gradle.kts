f52b521c5bc7b5ad0ed7e63935c1af11ee4bf6ac
jayshah193 <96729362+jayshah193@users.noreply.github.com>
2025-01-20 19:02:01 -0500

    FINAL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,60 @@
-buildscript {
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "ca.unb.mobiledev.fitquest"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "ca.unb.mobiledev.fitquest"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
     }
 }
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("nl.joery.timerangepicker:timerangepicker:1.0.0")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.mikhaellopez:circularprogressbar:3.1.0")
+
+
 }
